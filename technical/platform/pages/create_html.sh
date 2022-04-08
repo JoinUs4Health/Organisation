6b4dbd8e88c9a45bf04ce6d89196f3b07b23efd7
winterb <winterb@uni-greifswald.de>
2022-04-08 14:36:49 +0200

    minor script update

--- technical/platform/pages/create_html.sh
+++ technical/platform/pages/create_html.sh
@@ -2,8 +2,8 @@
 set -e
 pandoc terms-of-use_en.md -o terms-of-use_en.html
 pandoc terms-of-use_de.md -o terms-of-use_de.html
-#pandoc privacy_statement_en.md -o privacy_statement_en.html
-#pandoc privacy_statement_de.md -o privacy_statement_de.html
+pandoc privacy_statement_en.md -o privacy_statement_en.html
+pandoc privacy_statement_de.md -o privacy_statement_de.html
 pandoc legal_en.md -o legal_en.html
 pandoc legal_de.md -o legal_de.html
 
